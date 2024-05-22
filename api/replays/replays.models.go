7c549cd48218dc090f2c02b66a3c6b5663390011
NgYuKang <87468675+NgYuKang@users.noreply.github.com>
2024-05-22 13:13:07 +0800

    fix: change struct for list endpoint only

--- api/replays/replays.models.go
+++ api/replays/replays.models.go
@@ -1,16 +1,15 @@
 package replays
 
 import (
-	"Golang-Replay-REST/api/replaycomments"
-
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
+// Struct for List only
 type Replay struct {
-	ReplayID    int                             `json:"replayID"`
-	ReplayTitle string                          `json:"replayTitle"`
-	StageName   string                          `json:"stageName"`
-	CreatedAt   pgtype.Timestamp                `json:"createdAt"`
-	Likes       int                             `json:"likes"`
-	Comments    []replaycomments.ReplayComments `json:"comments"`
+	ReplayID    int              `json:"replayID"`
+	ReplayTitle string           `json:"replayTitle"`
+	StageName   string           `json:"stageName"`
+	CreatedAt   pgtype.Timestamp `json:"createdAt"`
+	Likes       int              `json:"likes"`
+	// Comments    []replaycomments.ReplayComments `json:"comments"` // NOT NEEDED FOR LIST
 }
