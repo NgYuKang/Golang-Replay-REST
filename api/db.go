85230eb0025a87b53f0058734b44a736bc0c6368
NgYuKang <87468675+NgYuKang@users.noreply.github.com>
2024-05-22 11:18:40 +0800

    feat: base db interface

---
+++ api/db.go
@@ -0,0 +1,15 @@
+package api
+
+// This is a base interface for db, copied from automatically generated sqlc files
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type DBTX interface {
+	Exec(context.Context, string, ...interface{}) (pgconn.CommandTag, error)
+	Query(context.Context, string, ...interface{}) (pgx.Rows, error)
+	QueryRow(context.Context, string, ...interface{}) pgx.Row
+}
